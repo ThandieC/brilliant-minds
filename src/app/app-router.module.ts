07fcca7b3c2c2fd738cba59cac68d722ac604ebc
thandienc <thandiec2@gmail.com>
2022-05-01 17:35:07 +0200

    initial commit part1

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './auth/auth-guard';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'stories',
+    loadChildren: () =>
+      import('./stories/stories.module').then((m) => m.StoriesModule),
+  },
+  {
+    path: 'creations',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./creations/creations.module').then((m) => m.CreationsModule),
+  },
+  { path: '**', redirectTo: '/home' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      preloadingStrategy: PreloadAllModules,
+      //initialNavigation: 'enabled',
+      onSameUrlNavigation: 'reload',
+    }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRouterModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { AppComponent } from './app.component';
+import { AppRouterModule } from './app-router.module';
+import { HeaderComponent } from './header/header.component';
+import { SharedModule } from './shared/shared.module';
+import * as fromApp from './store/app.reducer';
+import { FooterComponent } from './footer/footer.component';
+import { CreationsEffects } from './creations/store/creations.effects';
+import { AuthEffects } from './auth/store/auth.effects';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyAfZzWWs4pOiBgq0CocV6seE3mDkbyicTU',
+  authDomain: 'brilliant-minds-app.firebaseapp.com',
+  projectId: 'brilliant-minds-app',
+  storageBucket: 'brilliant-minds-app.appspot.com',
+  messagingSenderId: '716394966274',
+  appId: '1:716394966274:web:0e59081974757f54d6e61b',
+  measurementId: 'G-TXQHCVGGQ0',
+};
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([CreationsEffects, AuthEffects]),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
+    AppRouterModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
