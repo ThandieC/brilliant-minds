ef9127aeee0a660f29f6877022499ee22aa62720
thandienc <thandiec2@gmail.com>
2022-05-02 17:38:17 +0200

    updated Stories Library

--- src/app/stories-library/ThatSmartGirlInMyClass.ts
+++ src/app/stories-library/ThatSmartGirlInMyClass.ts
@@ -2,12 +2,61 @@ import { StoryModel } from '../shared/story.model';
 
 export const thatSmartGirl: StoryModel = {
   title: `That Smart Girl In My Class`,
-  description: 'You never know what to expect with Zoe the smart kid.',
+  description: 'She was the most interesting kid in my class.',
   storyBody: {
-    para1: `Zoe was really smart ...`,
+    para1: `Zoe was a really smart sixth grader. She could work out any Math problem
+    faster than any of us. She could memorize the multiplication
+    tables and recite them like they were her own name. She did not need a Science Textbook to
+     redo any of the experiments we had done before. We usually said her brain was like a sponge,
+     it absorbed and remembered everything she learned.`,
+
+    para2: ` Apart from being so intelligent, Zoe was so beautiful. She was tall and slim, light skinned
+    with shiny brown hair, and little dimples on her cheeks. She had bright white perfectly shaped
+    teeth that made her brilliant smile imprint a lasting impression on anyone, especially boys of course.
+     Her uniform was just the right size, and cascaded down her body curves in such an immaculate way she got stares
+     all around as she cruised softly down the school corridors.`,
+
+    para3: `Zoe was usually the last student to arrive in class, I do not
+      know if it was deliberate so everyone could admire her as she walked in and flashed a bright white shy smile before apologizing
+      to the teacher in her soft sweet voice, or maybe she had real delays that she could not avoid. But either way,
+       she hardly ever got detention like the rest of us. All the teachers liked her; for her beauty, or for her
+       intelligence, I do not know.`,
+
+    para4: `Though Zoe was such an outstandingly intelligent and beautiful girl, she had hardly any friends. No one
+    really liked sitting next to her in class or playing with her outside. Everyone thought she was uncomfortably too quiet, not the kind
+    of person you would like for a buddy. She was therefore always lonely, and that did not
+     seem to bother her at all. Her real friends were here books, and the Science Lab of course. Though I admired her so much, I also
+     felt sorry for her. Everyone needs a friend, no matter how smart, or quiet, they are. But Zoe seemed to live in her own world, where she was happy alone.`,
   },
   imageURL: 'assets/images/stories/girl-on-books.png',
-  imageAlt: 'Placeholder',
-  questions: [],
-  answers: [],
+  imageAlt: 'Girl sitting on books',
+  questions: [
+    '1. What is the name of the girl in the story?',
+    '2. What grade was Zoe in?',
+    "3. Zoe's classmates compared her brain to a what?",
+    '4. Zoe could recite Mathematics tables like her own what?',
+    '5. Complete: Apart from being intelligent, Zoe was also _________.',
+    '6. Complete: Zoe hardly had any friends because she was too _________.',
+    "7. Find another word in the story that has the same meaning as 'Intelligent'.",
+    '8. What is another word given to a person who is very quiet and keeps to herself?',
+    "9. Find another word in the story that has the same meaning as 'Beautiful'",
+    '10. In which tense is the story written in?',
+  ],
+  answers: [
+    'Zoe',
+    'Six',
+    'Sponge',
+    'Name',
+    'Beautiful',
+    'Quiet',
+    'Smart',
+    'Introverted',
+    'Immaculate',
+    'Past',
+    'Extroverted',
+    'Rubber',
+    'Present',
+    'Five',
+    'Mean',
+  ],
 };
--- src/app/stories-library/TheZebraWithNoStripes.ts
+++ src/app/stories-library/TheZebraWithNoStripes.ts
@@ -2,12 +2,72 @@ import { StoryModel } from '../shared/story.model';
 
 export const theZebraStory: StoryModel = {
   title: `The Zebra With No Stripes`,
-  description: 'Zippy the Zebra has to learn that being unique is special.',
+  description: 'Zippy the Zebra has to learn that being different is special.',
   storyBody: {
-    para1: `Zippy the Zebra ...`,
+    para1: `"Woo, what a night!" yawned Zippy the Zebra as he stretched out his neck over the tall grass and
+    looked around. The sun was up and shining ever so brighlty in a spotless blue sky.
+      A soft breeze gently combed though the tall brown savanna grass that Zippy called his home. His mom was
+      lazily munching grass nearby, occasionally stopping and gazing around. She was always on the lookout for
+      trouble. The day was beautiful, everything was perfect. Well, perfect except for the fact that Zippy did not
+      look anything like the rest of his zebra tribe.`,
+
+    para2: `Zippy leaped up out of the grass and stretched out his four legs. Then he turned and spotted a group of
+    animals playfully running around and munching on grass some distance away. She sighed and slumped back to the ground.`,
+
+    para3: `"What is wrong Zippy? You look so sad." Zippy's mum asked, a mouthful of grass hanging down from her mouth.`,
+
+    para4: `"If only I wasn't different. If only I looked just like all the other zebras." Zippy moaned. Unlike other
+     zebras, Zippy's coat was all black without a single white stripe.`,
+
+    para5: `"Zippy, you are perfect just the way you are." His mum said, walking over to him and hugging him with her neck.
+     "Everyone is unique and special in their own way. Our differences make us even more special."`,
+
+    para6: `"If that is the case, then why isn't anyone interested in playing with me. Everyone thinks I'm dull and stupid. No one
+     will ever want to play with me. They will just laugh at my plain black coat." Zippy continued his moarning.`,
+
+    para7: `"Why don't you go out there and try playing with others. You might just be surprised." His mum said, as she walked off towards
+      the water hole.`,
+
+    para8: `Zippy quietly slept under the shade of an Amarula tree, sorrowfully watching the other animals play while processing
+     in his mind what his mother had just said to him. Then, without giving it a second thought, he leaped up and trotted over
+     to the other animals, and stopped just as he raeched them.`,
+
+    para9: `"Hey, Zippy! Finally you join us." said the other animals excitedly. "Come over and play. Why don't you ever play with us?"`,
+
+    para10: `Zippy was surprised. Everyone was excited to have him around. No one even seemed to care about or notice his different coat. Zippy
+     ran and played and munched grass with his new friends the whole morning, and later they went off together to the
+     local water hole to quench their thirst. It turned out to be the best day ever for Zippy, and he learned a wonderful lesson
+     that day; that everyone was different and special in their own unique way.`,
   },
   imageURL: 'assets/images/stories/zebras-and-giraffe.png',
-  imageAlt: 'Placeholder',
-  questions: [],
-  answers: [],
+  imageAlt: 'Zebras and a giraffe',
+  questions: [
+    '1. What kind of animal was Zippy?',
+    '2. What do zebras feed on?',
+    '3. Zippy was different because his coat did not have what?',
+    '4. Did Zippy like his coat?',
+    '5. Zippy was sleeping under the shade of what tree?',
+    '6. How did the other animals feel when Zippy went to play with them?',
+    "7. What word in the story is the opposite of 'The same'?",
+    '8. Where did the animals get water to drink?',
+    '9. Complete: A normal zebra has black and _______ stripes.',
+    '10. Zippy learned that everyone was different and what?',
+  ],
+  answers: [
+    'Zebra',
+    'Grass',
+    'Stripes',
+    'No',
+    'Amarula',
+    'Excited',
+    'Different',
+    'Water Hole',
+    'White',
+    'Special',
+    'Yes',
+    'Leaves',
+    'Angry',
+    'River',
+    'Tree',
+  ],
 };
